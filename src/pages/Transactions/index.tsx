4231984b328ff0317b4f058cf01b64ced71589d9
Felipe Jung <felipemattoseu@gmail.com>
2024-07-25 13:20:53 -0400

    Add transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,32 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import * as S from "./styles";
+
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <S.TransactionsContainer>
+        <S.TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <S.PriceHighlight variant="income">R$ 12.000,00</S.PriceHighlight>
+              <td>Venda</td>
+              <td>25/07/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <S.PriceHighlight variant="outcome">- R$ 59,00</S.PriceHighlight>
+              <td>Alimentação</td>
+              <td>25/07/2024</td>
+            </tr>
+          </tbody>
+        </S.TransactionsTable>
+      </S.TransactionsContainer>
     </div>
   );
 }
